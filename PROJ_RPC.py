3d6d30838233b0c0dc449d8c869b45da851a5d16
kaosmagick <68394486+kaosmagick@users.noreply.github.com>
2021-09-23 11:58:50 +0545

    ADDED Multiple Case Support User Input in Lower, Mixed and Upper cases WORK

--- PROJ_RPC.py
+++ PROJ_RPC.py
@@ -4,25 +4,28 @@ print ("Welcome to the game!")
 
 
 
-var=("Rock", "Paper", "Scissors")
+var=("rock", "paper", "scissors")
+
+
 
 
 c=1
 while c>0:
     com=var[randint(0,2)]
     user=input("Enter your Move: ")   
+    user=user.lower()
     if user==("exit"):
         break 
 
     while com==var[0]:
         if user==var[0]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("It's a Draw")
         elif user==var[1]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("You Win!")
         elif user==var[2]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("Computer Wins!")
         else:
             print ("invalid input")    
@@ -32,13 +35,13 @@ while c>0:
 
     while com==var[1]:
         if user==var[1]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("It's a Draw")
         elif user==var[2]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("You Win!")
         elif user==var[0]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("Computer Wins!")
         else:
             print ("invalid input") 
@@ -49,13 +52,13 @@ while c>0:
 
     while com==var[2]:
         if user==var[2]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("It's a Draw")
         elif user==var[0]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("You Win!")
         elif user==var[1]:
-            print ("Computer: ", com)
+            print ("Computer: ", com.title())
             print ("Computer Wins!")
         else:
             print ("invalid input")    
